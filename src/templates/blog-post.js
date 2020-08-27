0f8842c02d61fcea315c430cbb96e6d17d8954c7
Hafiz Shehbaz Ali <shehbaz.bsee1395@gmail.com>
2020-08-28 03:57:45 +0400

    SEO added.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 export default function BlogPost({ data }) {
   const post = data.markdownRemark
   return (
     <Layout>
+      <SEO title={post.frontmatter.title} description={post.excerpt} />
       <div>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
@@ -20,6 +22,7 @@ export const query = graphql`
       frontmatter {
         title
       }
+      excerpt
     }
   }
 `
